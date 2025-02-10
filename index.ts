530400ac7b9886ea7368230de89c3942aeca0a49
Aurélien DE LA CASA <aurelien@stracker.fr>
2025-02-10 17:26:00 +0100

    changes feature

--- index.ts
+++ index.ts
@@ -1,20 +1,3 @@
-/*
-Feature: Data Source Join and Mapping
-
-Scenario: Joining and mapping data from third-party API and local DB query in execute()
-
-GIVEN two different data sources, one from a third-party API (call3partyAPI) and one from a local DB query (dbQuery)
-AND the call3partyAPI() returns static "curriculum" data
-AND the dbQuery() returns client learning progress data from our DB
-WHEN the execute() function is called
-THEN it should return an array of { ref, eventName, readableStatus }
-AND the readableStatus for each event is defined as follows:
-    1: pending
-    2: in_progress
-    3: delayed
-    4: completed
-*/
-
 async function call3partyAPI() {
   return {
     status: 200, // could be 400 or 500 sometimes
@@ -46,6 +29,17 @@ async function call3partyAPI() {
             {
               transportRef: "DEF456",
               name: "Available for collection",
+            },
+          ],
+        },
+      },
+      {
+        info: {
+          id: 3,
+          events: [
+            {
+              transportRef: "JKL567",
+              name: "Customs unavailable",
             },
           ],
         },
